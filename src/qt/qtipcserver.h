724435d266225804f89ab2da06ccdffadc9e735c
ehog0613 <ehog0613@gmail.com>
2018-06-01 15:22:11 +0900

    First commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define EcoPretium-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "EcoPretiumURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
